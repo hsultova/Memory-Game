03d31a8ede124a873c15dd82c09a536c3d5b95c1
hsultova <hsultova@gmail.com>
2016-03-27 18:23:59 +0300

    Create basic functionality for memory game
    -Fill and shuffle numbers from 1 to numberOfCards
    -Create table with width and length - sqrt(numberOfCards)

---
+++ memoryGame.js
@@ -0,0 +1,57 @@
+function main(numberOfCards)
+{
+	var table = [];
+	var outputTable;
+	var n = numberOfCards / 2; // Number of unique cards
+	var step = Math.sqrt(numberOfCards);
+	
+	fillTable(n);
+	
+	for (var i = 0; i < 10; i++)
+	{
+		shuffleTable();
+	}
+	
+	printTable();
+	document.getElementById("table").innerHTML = outputTable;
+	//console.log(outputTable);
+	
+	function printTable()
+	{
+		outputTable = '<table>';
+		for (var row = 0; row < 2 * n; row += step)
+		{
+			outputTable += '<tr>';
+			for (var col = 0; col < step; col++)
+			{
+				outputTable += '<td>' + table[row + col] + '</td>';
+			}
+			outputTable += '</tr>';
+		}
+		outputTable += '<br></table>';
+	}
+	
+	function fillTable(n)
+	{
+		for (var i = 1; i <= n; i++)
+		{
+			table.push(i);
+			table.push(i);
+		}
+	}
+	
+	function shuffleTable()
+	{
+		var randomNumber = 0;
+		var numberToMove;
+		for (var i = 0; i < n; i++)
+		{
+			randomNumber = Math.floor((Math.random() * (2 * n - 1)) + 0); // random number from 0 to 19
+			numberToMove = table[randomNumber];
+			table[randomNumber] = table[i];
+			table[i] = numberToMove;
+		}
+	}
+}
+
+main(36); // square even number
